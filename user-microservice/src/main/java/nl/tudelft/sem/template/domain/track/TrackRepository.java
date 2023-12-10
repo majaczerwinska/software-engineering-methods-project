082db04183eb79dc4c49c4f621244897e5c8d525
Yushan Shan <y.shan-4@student.tudelft.nl>
2023-12-10 15:58:34 +0100

    remove unnecessary functions and fix the data type

--- user-microservice/src/main/java/nl/tudelft/sem/template/domain/track/TrackRepository.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/domain/track/TrackRepository.java
@@ -20,55 +20,24 @@ public interface TrackRepository extends JpaRepository<Track, String> {
      * @param id the id to look for
      * @return track that have this id
      */
-    @Query("SELECT t FROM Track t WHERE t.id = :id")
-    Optional<Track> findById(@Param("id") String id);
+    Optional<Track> findById(String id);
 
     /**
      * find track that have this title in the input event.
      *
-     * @param t     the title to look for
+     * @param title     the title to look for
      * @param event the event where the track belongs to
      * @return track that have this title in the input event
      */
-    @Query("SELECT t FROM Track t WHERE t.title = :ti AND t.event = :event")
-    Optional<Track> findByTitleAndEvent(@Param("ti") String t, @Param("event") Event event);
-
+    Optional<Track> findByTitleAndEvent(Title title, ParentEvent event);
 
     /**
      * find list of track that have this title.
      *
-     * @param t     the title to look for
+     * @param title     the title to look for
      * @return list of track that have this title
      */
-    @Query("SELECT t FROM Track t WHERE t.title = :ti")
-    List<Track> findByTitle(@Param("ti") String t);
-
-    /**
-     * find list of track that have submission deadline before the input date.
-     *
-     * @param date the latest date for giving submission
-     * @return list of track that have submission deadline before the input date
-     */
-    @Query("SELECT t FROM Track t WHERE t.submitDeadline < :latestDate")
-    List<Track> findBySubmitDeadline(@Param("latestDate") Date date);
-
-    /**
-     * find list of track that have review deadline before the input date.
-     *
-     * @param date the latest date for giving review
-     * @return list of track that have review deadline before the input date
-     */
-    @Query("SELECT t FROM Track t WHERE t.reviewDeadline < :date")
-    List<Track> findByReviewDeadline(@Param("date") Date date);
-
-    /**
-     * find list of track that have the keyword in its description.
-     *
-     * @param keyword the word that should be i the description
-     * @return list of track that have the keyword in its description
-     */
-    @Query("SELECT t FROM Track t WHERE LOWER(t.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    List<Track> findByDescriptionContaining(@Param("keyword") String keyword);
+    List<Track> findByTitle(Title title);
 
     /**
      * find list of track that in the event.
@@ -76,8 +45,7 @@ public interface TrackRepository extends JpaRepository<Track, String> {
      * @param event the event where the track belongs to
      * @return list of track that in the event
      */
-    @Query("SELECT t FROM Track t WHERE t.event = :event")
-    List<Track> findByEvent(@Param("event") Event event);
+    List<Track> findByEvent(ParentEvent event);
 
     /**
      * check if event with input id exist.
@@ -85,24 +53,21 @@ public interface TrackRepository extends JpaRepository<Track, String> {
      * @param id the id to check for
      * @return true if event with input id exist
      */
-    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM Track t WHERE t.id = :id")
-    boolean existsById(@Param("id") long id);
+    boolean existsById(long id);
 
     /**
      * check if event with input title exist in the input event.
      *
-     * @param t     the title to check for
+     * @param title     the title to check for
      * @return true if event with input title exist in the input event
      */
-    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM Track t WHERE t.title = :title AND t.event = :event")
-    boolean existsByTitleInEvent(@Param("title") String t, @Param("event") Event event);
+    boolean existsByTitleInEvent(Title title, ParentEvent event);
 
     /**
      * check if event with input title exist.
      *
-     * @param t     the title to check for
+     * @param title     the title to check for
      * @return true if event with input title exist
      */
-    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM Track t WHERE t.title = :title")
-    boolean existsByTitle(@Param("title") String t);
+    boolean existsByTitle(Title title);
 }
